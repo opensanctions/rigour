66326e334ccaa3144146d016d61cacce3eac9ad0
Friedrich Lindenberg <friedrich@pudo.org>
2023-11-16 17:24:22 +0100

    move over scripts unicode range tags from normality

---
+++ rigour/text/scripts.py
@@ -0,0 +1,44 @@
+from functools import lru_cache
+from typing import Tuple
+
+from rigour.text.script_data import BLOCK_TAGS, ALPHABET, HISTORIC, FUNKY, LATIN
+
+
+@lru_cache(maxsize=5000)
+def char_tags(char: str) -> Tuple[int, ...]:
+    """Get the tags applicable to a particular character."""
+    codepoint = ord(char)
+    for start, end, tags in BLOCK_TAGS:
+        if start <= codepoint <= end:
+            return tags
+    return ()
+
+
+def is_modern_alphabet(word: str) -> bool:
+    """Check if a word is written in a modern alphabet. The term alphabet is
+    used in a narrow sense here: it includes only alphabets that have vowels and
+    are safely transliterated to latin. Basically: Cyrillic, Greek, Armenian,
+    and Latin."""
+    for char in word:
+        tags = char_tags(char)
+        if not len(tags):
+            continue
+        if ALPHABET not in tags:
+            return False
+        if HISTORIC in tags or FUNKY in tags:
+            return False
+    return True
+
+
+def is_latin(word: str) -> bool:
+    """Check if a word is written in the latin alphabet."""
+    for char in word:
+        tags = char_tags(char)
+        if not len(tags):
+            continue
+        if LATIN not in tags:
+            return False
+        # CHECK: I don't think this exists in practice:
+        # if HISTORIC in tags or FUNKY in tags:
+        #     return False
+    return True
---
+++ tests/text/test_scripts.py
@@ -0,0 +1,16 @@
+from rigour.text.scripts import is_latin
+from rigour.text.scripts import is_modern_alphabet
+
+
+def test_is_latin():
+    assert is_latin("banana")
+    assert not is_latin("банан")
+    assert is_latin("😋"), ord("😋")
+
+
+def test_is_modern_alphabet():
+    assert is_modern_alphabet("banana")
+    assert is_modern_alphabet("банан")
+    assert not is_modern_alphabet("中國哲學書電子化計劃")
+    assert not is_modern_alphabet("ᚠ")
+    assert is_modern_alphabet("😋")  # skips irrelevant blocks
