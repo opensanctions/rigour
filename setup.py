2dc42b66db4744aaf74d2db2085e1f6b1adcf9e1
Friedrich Lindenberg <friedrich@pudo.org>
2023-11-27 10:44:21 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="rigour",
-    version="0.1.0",
+    version="0.1.1",
     author="OpenSanctions",
     author_email="tech@opensanctions.org",
     url="https://followthemoney.tech/",
