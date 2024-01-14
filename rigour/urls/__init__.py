978f2fa79902dbd8f5c51d38210252602a0cf8fa
Friedrich Lindenberg <friedrich@pudo.org>
2024-01-14 12:41:46 +0100

    export paramstype

--- rigour/urls/__init__.py
+++ rigour/urls/__init__.py
@@ -1,4 +1,5 @@
 from rigour.urls.cleaning import clean_url, clean_url_compare, build_url
 from rigour.urls.compare import compare_urls
+from rigour.urls.util import ParamsType
 
-__all__ = ["clean_url", "clean_url_compare", "build_url", "compare_urls"]
+__all__ = ["clean_url", "clean_url_compare", "build_url", "compare_urls", "ParamsType"]
