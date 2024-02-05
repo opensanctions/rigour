79e8d56602aca14b4236fdec6944f148e3fc24f6
Friedrich Lindenberg <friedrich@pudo.org>
2024-02-05 15:55:10 +0100

    Bump version: 0.4.4 → 0.4.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 setup(
     name="rigour",
     description="Business data validation and normalization library.",
-    version="0.4.4",
+    version="0.4.5",
     author="OpenSanctions",
     author_email="tech@opensanctions.org",
     url="https://followthemoney.tech/",
