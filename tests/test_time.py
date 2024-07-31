ab76e2955e8705a78afb20120410b5edab978a3b
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-07-31 17:30:27 +0200

    add a helper for naive datetimes

--- tests/test_time.py
+++ tests/test_time.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone, date
-from rigour.time import utc_now, utc_date
+from rigour.time import utc_now, naive_now, utc_date
 
 
 def test_utc_now():
@@ -9,3 +9,11 @@ def test_utc_now():
     assert utc_date() is not None
     assert isinstance(utc_date(), date)
     assert utc_date() == utc_now().date()
+
+
+def test_naive_now():
+    assert naive_now() is not None
+    assert isinstance(naive_now(), datetime)
+    assert naive_now().tzinfo is None
+    assert naive_now().date() == utc_now().date()
+    assert naive_now().day == utc_now().day
