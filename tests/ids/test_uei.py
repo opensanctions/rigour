2b4e2484b4f2767159c130e4cf040a46c47d82e5
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-12-06 13:05:09 +0100

    introduce UEI, fixes #12

---
+++ tests/ids/test_uei.py
@@ -0,0 +1,11 @@
+from rigour.ids import UEI
+
+
+def test_uei():
+    assert UEI.is_valid("F5L4T8CUYC68")
+    assert not UEI.is_valid("F5L4T8CUYC")
+    assert not UEI.is_valid("05L4T8CUYC68")
+    assert not UEI.is_valid("F5L4T8IUYC68")
+    assert UEI.normalize("F5L4T8Cuyc68") == "F5L4T8CUYC68"
+    assert UEI.normalize("UEI F5L4T8Cuyc68") == "F5L4T8CUYC68"
+    assert UEI.format("F5L4T8Cuyc68") == "F5L4T8CUYC68"
