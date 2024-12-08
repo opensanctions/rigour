06d93aadbc533818dd8fbe5284ccb2724a19e41e
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-12-08 17:50:34 +0100

    function to tokenize names

--- rigour/names/tokenize.py
+++ rigour/names/tokenize.py
@@ -1,7 +1,11 @@
+import unicodedata
+from typing import Dict, List, Tuple
 from normality.constants import WS
 from normality.cleaning import category_replace, collapse_spaces
 from normality.util import Categories
 from fingerprints.cleanup import CHARACTERS_REMOVE_RE
+
+from rigour.text.distance import levenshtein
 
 TOKEN_SEP_CATEGORIES: Categories = {
     "Cc": WS,
@@ -35,6 +39,30 @@ def prepare_tokenize_name(text: str) -> str:
     """Prepare a name for tokenization."""
     text = text.lower()
     text = CHARACTERS_REMOVE_RE.sub("", text)
-    text = category_replace(text, replacements=TOKEN_SEP_CATEGORIES) or ''
+    text = category_replace(text, replacements=TOKEN_SEP_CATEGORIES) or ""
     text = collapse_spaces(text) or ""
     return text
+
+
+def tokenize_name(text: str, min_length: int = 1) -> List[str]:
+    """Split a person or entity's name into name parts."""
+    # FIXME: Do we want to support CJK scripts at some stage?
+    tokens: List[str] = []
+    token: List[str] = []
+    for char in text:
+        if char in ".'’":
+            continue
+        cat = unicodedata.category(char)
+        char = TOKEN_SEP_CATEGORIES.get(cat, char)
+        if char is None:
+            continue
+        if char == WS:
+            if len(token) >= min_length:
+                tokens.append("".join(token))
+            token.clear()
+            continue
+        token.append(char)
+
+    if len(token) >= min_length:
+        tokens.append("".join(token))
+    return tokens
