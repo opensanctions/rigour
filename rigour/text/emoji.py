20298e8b41d58027a458668b27b5183633de57e6
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-04-02 09:20:29 +0200

    fix emoji lib, was eating chinese characters

--- rigour/text/emoji.py
+++ rigour/text/emoji.py
@@ -6,8 +6,12 @@ EMOJI_PATTERN = re.compile(
     "\U0001f300-\U0001f5ff"  # symbols & pictographs
     "\U0001f680-\U0001f6ff"  # transport & map symbols
     "\U0001f1e0-\U0001f1ff"  # flags (iOS)
-    "\U00002702-\U000027b0"
-    "\U000024c2-\U0001f251"
+    "\u2600-\u26ff"
+    "\U0001f680-\U0001f6ff"  # transport & map symbols
+    "\U00002700-\U000027bf"  # dingbats
+    "\ufe0e\ufe0f"
+    "\U0001f3fb-\U0001f3ff"  # skin tones
+    "\U0001f9c0-\U0001f9ff"  # additional symbols
     "]+",
     flags=re.UNICODE,
 )
--- tests/text/test_emoji.py
+++ tests/text/test_emoji.py
@@ -6,3 +6,5 @@ def test_remove_emoji():
     assert remove_emoji("ab⚔️🚩cd") == "abcd"
     assert remove_emoji("\U0001f600\U0001f601") == ""
     assert remove_emoji("ЙГЗЖ") == "ЙГЗЖ"
+
+    assert remove_emoji("卢拉玛·克辛瓜纳") == "卢拉玛·克辛瓜纳"
