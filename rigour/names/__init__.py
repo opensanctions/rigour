88750902961803ec697483c35a6b9c94946e132d
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-04-02 18:10:07 +0200

    introduce "pick_case" function for names

--- rigour/names/__init__.py
+++ rigour/names/__init__.py
@@ -2,9 +2,9 @@
 Name handling utilities for person and organisation names.
 """
 
-from rigour.names.pick import pick_name
+from rigour.names.pick import pick_name, pick_case
 from rigour.names.part import name_parts
 from rigour.names.check import is_name
 from rigour.names.tokenize import tokenize_name
 
-__all__ = ["pick_name", "name_parts", "is_name", "tokenize_name"]
+__all__ = ["pick_name", "pick_case", "name_parts", "is_name", "tokenize_name"]
