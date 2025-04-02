7736daa64a03788036f36d0ed33716f55596c378
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-04-02 09:20:58 +0200

    little library to check if a string can be a name (needs on letter-char)

--- rigour/names/__init__.py
+++ rigour/names/__init__.py
@@ -4,6 +4,7 @@ Name handling utilities for person and organisation names.
 
 from rigour.names.pick import pick_name
 from rigour.names.part import name_parts
+from rigour.names.check import is_name
 from rigour.names.tokenize import tokenize_name
 
-__all__ = ["pick_name", "name_parts", "tokenize_name"]
+__all__ = ["pick_name", "name_parts", "is_name", "tokenize_name"]
---
+++ tests/names/test_check.py
@@ -0,0 +1,10 @@
+from rigour.names.check import is_name
+
+
+def test_is_name():
+    assert is_name("John")
+    assert not is_name("12345")
+    assert is_name("A")
+    assert not is_name("")
+    assert not is_name(" ")
+    assert not is_name("---")
