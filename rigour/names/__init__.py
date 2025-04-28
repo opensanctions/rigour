e1df1545cccd527866a741e99674541149411626
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-04-28 17:57:10 +0200

    export name prefix removal properly

--- rigour/names/__init__.py
+++ rigour/names/__init__.py
@@ -18,6 +18,7 @@ from rigour.names.tag import NamePartTag, NameTypeTag
 from rigour.names.pick import pick_name, pick_case
 from rigour.names.check import is_name
 from rigour.names.tokenize import tokenize_name
+from rigour.names.person import remove_person_prefixes
 from rigour.names.org_types import replace_org_types_display
 from rigour.names.org_types import replace_org_types_compare
 from rigour.names.org_types import extract_org_types
@@ -31,6 +32,7 @@ __all__ = [
     "NamePart",
     "NamePartTag",
     "NameTypeTag",
+    "remove_person_prefixes",
     "replace_org_types_display",
     "replace_org_types_compare",
     "extract_org_types",
