de73acfc5ba5c9086e1c2076c964c84e92d9875e
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-05-14 16:11:58 +0100

    add awkawardly-named maybe ascii to part

--- tests/names/test_part.py
+++ tests/names/test_part.py
@@ -4,6 +4,7 @@ from rigour.names.part import NamePart
 def test_name_part():
     john = NamePart("john", 0)
     assert john.ascii == "john"
+    assert john.maybe_ascii == "john"
     assert john.form == "john"
     assert john.metaphone == "JN"
     assert john.is_modern_alphabet is True
@@ -15,11 +16,13 @@ def test_name_part():
 
     petro = NamePart("Петро́", 0)
     assert petro.ascii == "Petro"
+    assert petro.maybe_ascii == "Petro"
     assert petro.metaphone == "PTR"
     assert petro != john
     assert petro != 3
 
     osama = NamePart("أسامة", 0)
     assert osama.ascii == "asamt"
+    assert osama.maybe_ascii == "أسامة"
     assert osama.is_modern_alphabet is False
     assert osama.metaphone is None
