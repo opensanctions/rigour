2455abf1428066442005901e1c98c5f94ab2e3f1
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-06-01 16:58:10 +0200

    Move in alignment code for name parts

---
+++ rigour/names/alignment.py
@@ -0,0 +1,121 @@
+from typing import List, Optional
+
+from rigour.names.part import NamePart
+from rigour.names.tag import FAMILY_NAME_TAGS, GIVEN_NAME_TAGS, NamePartTag
+from rigour.text.distance import levenshtein_similarity
+
+
+class Alignment:
+    """Data object to hold the alignment of name parts between query and result."""
+
+    def __init__(self) -> None:
+        self.query_sorted: List[NamePart] = []
+        self.result_sorted: List[NamePart] = []
+        self.query_extra: List[NamePart] = []
+        self.result_extra: List[NamePart] = []
+
+    def __len__(self) -> int:
+        return max(len(self.query_sorted), len(self.result_sorted))
+
+
+def align_name_slop(
+    query: List[NamePart], result: List[NamePart], max_slop: int = 2
+) -> Alignment:
+    """Align name parts of companies and organizations. The idea here is to allow
+    skipping tokens within the entity name if this improves overall match quality,
+    but never to re-order name parts. The resulting alignment will contain the
+    sorted name parts of both the query and the result, as well as any extra parts
+    that were not aligned.
+
+    Note that one name part in one list may correspond to multiple name parts in the
+    other list, so the alignment is not necessarily one-to-one.
+
+    The `levenshtein` distance is used to determine the best alignment, allowing
+    for a certain spelling variation between the names.
+
+    Args:
+        query (List[NamePart]): The name parts of the query.
+        result (List[NamePart]): The name parts of the result.
+        max_slop (int): The maximum number of tokens that can be skipped in the
+            alignment. Defaults to 2.
+    Returns:
+        Alignment: An object containing the aligned name parts and any extra parts.
+    """
+    alignment = Alignment()
+    if len(query) == 0 or len(result) == 0:
+        return alignment
+
+    # TODO: the programming
+    # see test_alignment.py for examples
+    return alignment
+
+
+def _check_align_tags(query: NamePart, result: NamePart) -> bool:
+    """
+    Check if the tags of the query and result name parts can be aligned.
+
+    Args:
+        query (NamePart): The name part from the query.
+        result (NamePart): The name part from the result.
+
+    Returns:
+        bool: True if the tags can be aligned, False otherwise.
+    """
+    if NamePartTag.ANY in (query.tag, result.tag):
+        return True
+    if query.tag in GIVEN_NAME_TAGS and result.tag in FAMILY_NAME_TAGS:
+        return False
+    if query.tag in FAMILY_NAME_TAGS and result.tag in GIVEN_NAME_TAGS:
+        return False
+    return True
+
+
+def align_person_name_order(query: List[NamePart], result: List[NamePart]) -> Alignment:
+    """Aligns the name parts of a person name for the query and result based on their
+    tags and their string similarity such that the most similar name parts are matched.
+
+    Args:
+        query (List[NamePart]): The name parts from the query.
+        result (List[NamePart]): The name parts from the result.
+
+    Returns:
+        Alignment: An object containing the aligned name parts and any extra parts.
+    """
+    alignment = Alignment()
+
+    for qpart in sorted(query, key=len, reverse=True):
+        best_match: Optional[NamePart] = None
+        best_score = 0.0
+        for rpart in sorted(result, key=len, reverse=True):
+            if rpart in alignment.result_sorted:
+                continue
+            if not _check_align_tags(qpart, rpart):
+                continue
+            min_len = min(len(qpart.maybe_ascii), len(rpart.maybe_ascii))
+            score = levenshtein_similarity(
+                qpart.maybe_ascii,
+                rpart.maybe_ascii,
+                max_edits=min_len // 2,
+                max_percent=1.0,
+            )
+            score = score * min_len
+            if score > best_score:
+                best_score = score
+                best_match = rpart
+
+        if best_match is not None:
+            alignment.query_sorted.append(qpart)
+            alignment.result_sorted.append(best_match)
+        else:
+            alignment.query_extra.append(qpart)
+
+    if not len(alignment.query_sorted):
+        alignment.query_sorted = query
+        alignment.result_sorted = result
+        return alignment
+
+    for rpart in result:
+        if rpart not in alignment.result_sorted:
+            alignment.result_extra.append(rpart)
+
+    return alignment
---
+++ tests/names/test_alignment.py
@@ -0,0 +1,125 @@
+from typing import List
+from rigour.names.name import Name
+from rigour.names.part import NamePart
+from rigour.names.alignment import align_name_slop, align_person_name_order
+from rigour.names.tag import NamePartTag
+
+
+def make(name: str) -> List[NamePart]:
+    obj = Name(name, form=name.lower())
+    return obj.parts
+
+
+def test_align_name_slop():
+    query = make("Deutsche Bank AG")
+    result = make("Deutsche Bank Aktiengesellschaft")
+    amt = align_name_slop(query, result, max_slop=2)
+    assert amt is not None
+
+    # Example test cases:
+    # Deutsche Bank AG vs Deutsche Bank Aktiengesellschaft
+    # Deutsche Bank AG vs Deutsche Bahn AG
+    # Deutsche Bank (Schweiz) AG vs Deutsche Bank AG
+    # Deutsche Bank (Schweiz) AG vs Deutsche Bank Aktiengesellschaft
+    # Al-Haramain Foundation vs Al-Haramain Benevolent Foundation
+    # Production Enterprise NOVI GAZMASH vs NOVY GAZMASH
+    # Production Enterprise NOVI GAZMASH vs NOVIY GASMASH
+
+    # Meant to fail:
+    # Deutsche Bank AG vs Bank Deutsch AG
+    # NOVY GAZMASH vs GAZMASH NOVY
+
+
+def test_align_person_name_order():
+    query = make("John Doe")
+    result = make("Doe, John")
+    amt = align_person_name_order(query, result)
+    assert len(amt.query_sorted) == 2
+    assert len(amt.result_sorted) == 2
+    assert amt.query_sorted[0].form == "john"
+    assert amt.query_sorted[1].form == "doe"
+    assert amt.result_sorted[0].form == "john"
+    assert amt.result_sorted[1].form == "doe"
+
+    query = make("John Dow")
+    amt = align_person_name_order(query, result)
+    assert len(amt.query_sorted) == 2
+    assert amt.query_sorted[0].form == "john"
+    assert amt.query_sorted[1].form == "dow"
+    assert amt.result_sorted[0].form == "john"
+    assert amt.result_sorted[1].form == "doe"
+
+    query = make("John Richard Smith")
+    result = make("Smith, John")
+    amt = align_person_name_order(query, result)
+
+    assert len(amt.query_sorted) == 2
+    assert amt.query_sorted[0].form == "smith"
+    assert amt.query_sorted[1].form == "john"
+    assert amt.query_extra[0].form == "richard"
+    assert amt.result_sorted[0].form == "smith"
+    assert amt.result_sorted[1].form == "john"
+
+    query = make("John Richard Smyth")
+    amt = align_person_name_order(query, result)
+
+    assert len(amt.query_sorted) == 2
+    assert amt.query_sorted[0].form == "smyth"
+    assert amt.query_sorted[1].form == "john"
+    assert amt.query_extra[0].form == "richard"
+
+    # TODO:
+    # Ali Al-Sabah vs Ali Alsabah
+    # Ali Al-Sabah vs Alsabah, Ali
+    # Mohammed Abd Al-Rahman vs Abdalrahman, Mohammed
+
+
+def test_align_tagged_person_name_parts():
+    query = [
+        NamePart("john", 0, NamePartTag.GIVEN),
+        NamePart("smith", 1, NamePartTag.FAMILY),
+    ]
+    result = [
+        NamePart("john", 0, NamePartTag.GIVEN),
+        NamePart("smith", 1, NamePartTag.FAMILY),
+    ]
+    aligned = align_person_name_order(query, result)
+    assert len(aligned) == 2
+    assert aligned.query_sorted[1].form == "john"
+    assert aligned.result_sorted[1].form == "john"
+    assert aligned.query_sorted[0].form == "smith"
+    assert aligned.result_sorted[0].form == "smith"
+    query = [
+        NamePart("smith", 0, NamePartTag.FAMILY),
+        NamePart("john", 1, NamePartTag.GIVEN),
+    ]
+    aligned = align_person_name_order(query, result)
+    assert len(aligned) == 2, (aligned.query_sorted, aligned.result_sorted)
+    assert aligned.query_sorted[0].form == aligned.result_sorted[0].form
+
+    query = [
+        NamePart("smith", 0, NamePartTag.ANY),
+        NamePart("john", 1, NamePartTag.ANY),
+    ]
+    aligned = align_person_name_order(query, result)
+    assert len(aligned) == 2
+    assert aligned.query_sorted[1].form == aligned.result_sorted[1].form
+    assert aligned.query_sorted[0].form == aligned.result_sorted[0].form
+
+    query = [
+        NamePart("henry", 1, NamePartTag.GIVEN),
+        NamePart("smith", 0, NamePartTag.ANY),
+        NamePart("john", 1, NamePartTag.GIVEN),
+    ]
+    aligned = align_person_name_order(query, result)
+    assert len(aligned) == 2
+    assert len(aligned.query_extra) == 1
+
+    query = [
+        NamePart("smith", 0, NamePartTag.GIVEN),
+        NamePart("john", 1, NamePartTag.FAMILY),
+    ]
+    aligned = align_person_name_order(query, result)
+    assert len(aligned) == 2, (aligned.query_sorted, aligned.result_sorted)
+    assert aligned.query_sorted[0].form != aligned.result_sorted[0].form
+    assert aligned.query_sorted[1].form != aligned.result_sorted[1].form
