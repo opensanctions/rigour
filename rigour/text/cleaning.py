5b3dd2816e100690f12aa3b10d839151ef0ff0cd
Confirmordeny <mrjohncross@googlemail.com>
2025-07-27 08:37:51 +0100

    Update cleaning.py
    adding missing zeros and better coverage of mathematical symbols

--- rigour/text/cleaning.py
+++ rigour/text/cleaning.py
@@ -13,46 +13,47 @@ EMOJI_PATTERN = re.compile(
     "\U00002700-\U000027bf"  # dingbats
     "\U0001f1e0-\U0001f1ff"  # flags (iOS)
     "\U0001f300-\U0001f5ff"  # symbols and pictographs
-    "\U2600-\U26ff" # miscellaneous symbols block (including warning signs and symbols re weather, games, astrology and religion/culture)
+    "\U0002600-\U00026ff" # miscellaneous symbols block (including warning signs and symbols re weather, games, astrology and religion/culture)
     "\U0001f680-\U0001f6ff"  # transport and map symbols
-    "\U2b00-\U2bff" # miscellaneous symbols and arrows
-    "\U1f650-\U1f67f" # ornamental dingbats
-    "\U1f700-\U1f77f" # alchemical symbols
-    "\U1f780-\U1f7ff" # geometric shapes extended
-    "\U1f900-\U1f9ff" # supplemental symbols and pictographs
+    "\U0002b00-\U0002bff" # miscellaneous symbols and arrows
+    "\U0001f650-\U0001f67f" # ornamental dingbats
+    "\U0001f700-\U0001f77f" # alchemical symbols
+    "\U0001f780-\U0001f7ff" # geometric shapes extended
+    "\U0001f900-\U0001f9ff" # supplemental symbols and pictographs
     # Mathematical, computing and technical symbols 
-    "\U2190-\U21ff" # arrows
-    "\U2300-\U23ff" # miscellaneous technical
-    "\U2400-\U243f" # control pictures
-    "\U2440-\U245f" # optical character recognition
-    "\U2500-\U257f" # box drawing
-    "\U2580-\U259f" # block elements
-    "\U25a0-\U25ff" # geometric shapes
-    "\U2a00-\U2aff" # supplemental mathematical operators
+    "\U0002190-\U00021ff" # arrows
+    "\U0002300-\U00023ff" # miscellaneous technical
+    "\U0002400-\U000243f" # control pictures
+    "\U0002440-\U000245f" # optical character recognition
+    "\U0002500-\U000257f" # box drawing
+    "\U0002580-\U000259f" # block elements
+    "\U00025a0-\U00025ff" # geometric shapes
+    "\U0002200-\U00022ff" # mathematical operators and symbols
+    "\U0002a00-\U0002aff" # supplemental mathematical operators
     "\U0001FB00-\U0001FBFF" # symbols for legacy computing
     # Games, music, and esoteric symbols 
-    "\U4dc0-\U4dff" # Yijing hexagram symbols
-    "\U1d100-\U1d1ff" # musical symbols
-    "\U1d300-\U1d35f" # Tai Xuan Jing symbols
-    "\U1d360-\U1d37f" # counting rod numerals (used in ancient China, Japan, Korea, and Vietnam)
-    "\U1f000-\U1f02f" # Mahjong tiles
-    "\U1f030-\U1f09f" # domino tiles
-    "\U1f0a0-\U1f0ff" # playing cards
+    "\U0004dc0-\U0004dff" # Yijing hexagram symbols
+    "\U0001d100-\U0001d1ff" # musical symbols
+    "\U0001d300-\U0001d35f" # Tai Xuan Jing symbols
+    "\U0001d360-\U0001d37f" # counting rod numerals (used in ancient China, Japan, Korea, and Vietnam)
+    "\U0001f000-\U0001f02f" # Mahjong tiles
+    "\U0001f030-\U0001f09f" # domino tiles
+    "\U0001f0a0-\U0001f0ff" # playing cards
     # Ancient/obsolete scripts 
-    "\U16a0-\U16ff" # Runic
-    "\U1680-\U169f" # Ogham
-    "\U10330-\U1034f" # Gothic
-    "\U10900-\U1091f" # Phoenician
-    "\U10380-\U1039f" # Ugaritic (an extinct Semitic language)
-    "\U103a0-\U103df" # Old Persian
-    "\U10000-\U100ff" # Linear B (used for an form of the Greek language)
-    "\U12000-\U123ff" # Cuneiform
-    "\U13000-\U1342f" # Egyptian hieroglyphs
-    "\U14400-\U1467f" # Anatolian hieroglyphs
+    "\U00016a0-\U00016ff" # Runic
+    "\U0001680-\U000169f" # Ogham
+    "\U00010330-\U0001034f" # Gothic
+    "\U00010900-\U0001091f" # Phoenician
+    "\U00010380-\U0001039f" # Ugaritic (an extinct Semitic language)
+    "\U000103a0-\U000103df" # Old Persian
+    "\U00010000-\U000100ff" # Linear B (used for an form of the Greek language)
+    "\U00012000-\U000123ff" # Cuneiform
+    "\U00013000-\U0001342f" # Egyptian hieroglyphs
+    "\U00014400-\U0001467f" # Anatolian hieroglyphs
     # Constructed languages/scripts 
-    "\U10450-\U1047f" # Shavian (constructed script created in the 1950s and 1960s to write English)
-    "\U10400-\U1044f" # Deseret (developed in the 19th century by the Mormon Church for English)
-    "\U11a00-\U11a4f" # Zanabazar square script (17th century script for Tibetan and Sanskrit, now decorative)
+    "\U00010450-\U0001047f" # Shavian (constructed script created in the 1950s and 1960s to write English)
+    "\U00010400-\U0001044f" # Deseret (developed in the 19th century by the Mormon Church for English)
+    "\U00011a00-\U00011a4f" # Zanabazar square script (17th century script for Tibetan and Sanskrit, now decorative)
     "]+",
     flags=re.UNICODE,
 )
