fee38128a6d776904e7f1f5e1702ab344773a2f5
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-08-05 12:19:20 +0200

    make range specifications have the same length

--- rigour/text/cleaning.py
+++ rigour/text/cleaning.py
@@ -2,47 +2,47 @@ import re
 
 from normality.constants import WS
 
-EMOJI_PATTERN = re.compile(
+RANGE_PATTERN = re.compile(
     "["
     # Emojis
     "\U0001f600-\U0001f64f"  # emoticons
     "\U0001f3fb-\U0001f3ff"  # skin tones
-    "\ufe0e-\ufe0f" # variation selectors (these allow emoji and non-emoji forms of the same symbol to be selected)
-    # Symbols, shapes, and pictographs 
+    "\ufe0e-\ufe0f"  # variation selectors (these allow emoji and non-emoji forms of the same symbol to be selected)
+    # Symbols, shapes, and pictographs
     "\U00002700-\U000027bf"  # dingbats
     "\U0001f1e0-\U0001f1ff"  # flags (iOS)
     "\U0001f300-\U0001f5ff"  # symbols and pictographs (range also includes skin tones which is also covered above)
-    "\U0002600-\U00026ff" # miscellaneous symbols block (including warning signs and symbols re weather, games, astrology and religion/culture)
+    "\U00002600-\U000026ff"  # miscellaneous symbols block (including warning signs and symbols re weather, games, astrology and religion/culture)
     "\U0001f680-\U0001f6ff"  # transport and map symbols
-    "\U0002b00-\U0002bff" # miscellaneous symbols and arrows
-    "\U0001f650-\U0001f67f" # ornamental dingbats
-    "\U0001f700-\U0001f77f" # alchemical symbols
-    "\U0001f780-\U0001f7ff" # geometric shapes extended
-    "\U0001f900-\U0001f9ff" # supplemental symbols and pictographs
+    "\U00002b00-\U00002bff"  # miscellaneous symbols and arrows
+    "\U0001f650-\U0001f67f"  # ornamental dingbats
+    "\U0001f700-\U0001f77f"  # alchemical symbols
+    "\U0001f780-\U0001f7ff"  # geometric shapes extended
+    "\U0001f900-\U0001f9ff"  # supplemental symbols and pictographs
     # Musical symbols
-    "\U0001d100-\U0001d1ff" # musical symbols
-    "\U0001d200-\U0001d24f" # Ancient Greek musical notation
-    "\U0001d000-\U0001d0ff" # Byzantine musical symbols
-    "\U0001cf00-\U0001cfcf" # Znamenny musical notation
-    # Mathematical, computing and technical symbols 
-    "\U0002190-\U00021ff" # arrows
-    "\U0002300-\U00023ff" # miscellaneous technical
-    "\U0002400-\U000243f" # control pictures
-    "\U0002440-\U000245f" # optical character recognition
-    "\U0002500-\U000257f" # box drawing
-    "\U0002580-\U000259f" # block elements
-    "\U00025a0-\U00025ff" # geometric shapes
-    "\U0002200-\U00022ff" # mathematical operators and symbols
-    "\U0002a00-\U0002aff" # supplemental mathematical operators
-    "\U0001FB00-\U0001FBFF" # symbols for legacy computing
+    "\U0001d100-\U0001d1ff"  # musical symbols
+    "\U0001d200-\U0001d24f"  # Ancient Greek musical notation
+    "\U0001d000-\U0001d0ff"  # Byzantine musical symbols
+    "\U0001cf00-\U0001cfcf"  # Znamenny musical notation
+    # Mathematical, computing and technical symbols
+    "\U00002190-\U000021ff"  # arrows
+    "\U00002300-\U000023ff"  # miscellaneous technical
+    "\U00002400-\U0000243f"  # control pictures
+    "\U00002440-\U0000245f"  # optical character recognition
+    "\U00002500-\U0000257f"  # box drawing
+    "\U00002580-\U0000259f"  # block elements
+    "\U000025a0-\U000025ff"  # geometric shapes
+    "\U00002200-\U000022ff"  # mathematical operators and symbols
+    "\U00002a00-\U00002aff"  # supplemental mathematical operators
+    "\U0001fb00-\U0001fbff"  # symbols for legacy computing
     # Symbols used for games
-    "\U0001f000-\U0001f02f" # Mahjong tiles
-    "\U0001f030-\U0001f09f" # domino tiles
-    "\U0001f0a0-\U0001f0ff" # playing cards
+    "\U0001f000-\U0001f02f"  # Mahjong tiles
+    "\U0001f030-\U0001f09f"  # domino tiles
+    "\U0001f0a0-\U0001f0ff"  # playing cards
     # Shorthand
-    "\U0001BC00-\U0001BC9f" # Duployan shorthand
+    "\U0001bc00-\U0001bc9f"  # Duployan shorthand
     # Unicode specials block
-    "\ufff0-\uffff" # character for encoding errors (�) etc
+    "\ufff0-\uffff"  # character for encoding errors (�) etc
     "]+",
     flags=re.UNICODE,
 )
@@ -62,7 +62,7 @@ def remove_emoji(string: str) -> str:
     Returns:
         Text that doesn't include those.
     """
-    return EMOJI_PATTERN.sub(r"", string)
+    return RANGE_PATTERN.sub(r"", string)
 
 
 def remove_bracketed_text(text: str) -> str:
