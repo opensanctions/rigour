d71051d5e04a30d0f5eb1be4de51e83fc213d53b
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-08-28 16:49:44 +0200

    Simple text hashing function

---
+++ rigour/text/checksum.py
@@ -0,0 +1,24 @@
+from hashlib import sha1
+from typing import List
+from unicodedata import normalize
+from normality.cleaning import remove_unsafe_chars
+
+from rigour.env import ENCODING
+
+
+def text_hash(text: str) -> str:
+    """Generate a hash for the given text, ignoring whitespace and punctuation.
+
+    Args:
+        text (str): The input text to hash.
+
+    Returns:
+        str: The SHA-1 hash of the processed text.
+    """
+    substantial: List[str] = []
+    text = normalize("NFKC", remove_unsafe_chars(text.lower()))
+    for char in text:
+        if char.isalnum():
+            substantial.append(char)
+    text = "".join(substantial)
+    return sha1(text.encode(ENCODING)).hexdigest()
