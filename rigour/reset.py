7e0b1f27ef42ca6d75b30961ef91c91baa7832b6
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-12-19 11:49:38 +0100

    Clear more caches

--- rigour/reset.py
+++ rigour/reset.py
@@ -9,7 +9,9 @@ from rigour.territories.lookup import _get_identifier_map, _get_territory_names
 from rigour.text.scripts import can_latinize_cp
 from rigour.names import normalize_name
 from rigour.addresses.format import _load_formats, _load_template
+from rigour.addresses.normalize import _address_replacer
 from rigour.names.org_types import _compare_replacer, _display_replacer
+from rigour.names.org_types import _generic_replacer, replace_org_types_compare
 from rigour.names.tagging import _get_org_tagger, _get_person_tagger
 
 
@@ -27,8 +29,11 @@ def reset_caches() -> None:
     can_latinize_cp.cache_clear()
     _load_formats.cache_clear()
     _load_template.cache_clear()
+    _address_replacer.cache_clear()
     _compare_replacer.cache_clear()
     _display_replacer.cache_clear()
+    _generic_replacer.cache_clear()
+    replace_org_types_compare.cache_clear()
     _get_org_tagger.cache_clear()
     _get_person_tagger.cache_clear()
     get_index.cache_clear()
